3fcbb66b461a2b012c1560624815abf7d233fdfc
Debasish Mallick <debasish.mallick@gmail.com>
2025-07-15 08:34:10 +0530

    Created an endpoint to handle the user request. Next: UI and intergration

--- ai_agent.py
+++ ai_agent.py
@@ -11,21 +11,41 @@ from langchain_groq import ChatGroq
 from langchain_openai import ChatOpenAI
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain import hub
+from langchain.agents import AgentExecutor
+from langchain import hub
+from langchain.agents import AgentExecutor, create_react_agent
+
+def get_response(model_name, model_provider, system_prompt, messages, allow_search):
+    """
+    Function to get a response from the agent.
+    """
+    try:
+        #model Used: llama3-70b-8192
+        groq_llm = ChatGroq(model =model_name, temperature=0.0, api_key=GROQ_API_KEY)
+        search_tool = TavilySearchResults(max_results=3, TAVILY_API_KEY=TAVILY_API_KEY)
+        tools = [search_tool]
+       #os.environ["LANGCHAIN_TRACING_V2"] = "false"
+
+
+        prompt = hub.pull("hwchase17/react")
+        agent = create_react_agent(groq_llm, tools, prompt)
+        agent_executor = AgentExecutor(agent=agent, tools=tools)
+
+        respose = agent_executor.invoke({"input": "Tell me about the crypto currency market?"})
+        return respose["output"]
+    except Exception as e:
+        return str(e)
 
 #openai_llm = ChatOpenAI(model="gpt-4o-mini", temperature=0.0, api_key=OPENAI_API_KEY)
-groq_llm = ChatGroq(model ="llama3-70b-8192", temperature=0.0, api_key=GROQ_API_KEY)
+"""groq_llm = ChatGroq(model ="llama3-70b-8192", temperature=0.0, api_key=GROQ_API_KEY)
 search_tool = TavilySearchResults(max_results=3, TAVILY_API_KEY=TAVILY_API_KEY)
-#from langgraph.prebuilt import create_react_agent
-from langchain.agents import AgentExecutor
 tools = [search_tool]
-from langchain import hub
-#from langchain_community.llms import OpenAI
-from langchain.agents import AgentExecutor, create_react_agent
 os.environ["LANGCHAIN_TRACING_V2"] = "false"
+
 
 prompt = hub.pull("hwchase17/react")
 agent = create_react_agent(groq_llm, tools, prompt)
 agent_executor = AgentExecutor(agent=agent, tools=tools)
 
 respose = agent_executor.invoke({"input": "Tell me about the crypto currency market?"})
-print(respose["output"])
+print(respose["output"])"""
---
+++ backend.py
@@ -0,0 +1,39 @@
+from pydantic import BaseModel
+class RequestData(BaseModel):
+    model_name: str
+    model_provider: str
+    system_prompt: str
+    messages: list
+    allow_search: bool
+
+from fastapi import FastAPI, HTTPException
+from ai_agent import get_response
+app = FastAPI(title="Agentica Demo API", version="1.0")
+
+ALLOWED_MODELS = ["llama3-70b-8192", "gpt-3.5-turbo", "groq-llama2-70b"]
+
+@app.post("/chat")
+def chat_endpoint(request_data: RequestData):
+    print("Received request data:", request_data)
+    try:
+        # Here you would typically process the request_data
+        # and return a response based on the model_name and messages.
+        #return {"status": "success", "data": "Chat response goes here"}
+        if request_data.model_name not in ALLOWED_MODELS:
+            raise HTTPException(status_code=400, detail="Model not allowed")
+        else:
+            response = get_response(
+                model_name=request_data.model_name,
+                model_provider=request_data.model_provider,
+                system_prompt=request_data.system_prompt,
+                messages=request_data.messages,
+                allow_search=request_data.allow_search
+            )
+            return {"status": "success", "data": response}  
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e)) 
+    
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8090)   
